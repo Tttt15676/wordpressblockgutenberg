8f035b449e8f1870fe42d0aedbd428b9a1bbfd8d
Tttt15676 <118089777+Tttt15676@users.noreply.github.com>
2026-05-26 10:38:21 -0500

    Update edit.js

--- my-custom-block/src/my-custom-block/edit.js
+++ my-custom-block/src/my-custom-block/edit.js
@@ -1,41 +1,35 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
+import { useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, TextControl } from '@wordpress/components';
 
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
+export default function Edit( { attributes, setAttributes } ) {
+    const { heading, content, linkUrl } = attributes;
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
-	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'My Custom Block – hello from the editor!',
-				'my-custom-block'
-			) }
-		</p>
-	);
-}
+    return (
+        <>
+            <InspectorControls>
+                <PanelBody title={ __( 'Link Settings', 'my-custom-block' ) }>
+                    <TextControl
+                        label={ __( 'Link URL', 'my-custom-block' ) }
+                        value={ linkUrl }
+                        onChange={ ( value ) => setAttributes( { linkUrl: value } ) }
+                    />
+                </PanelBody>
+            </InspectorControls>
+            <div { ...useBlockProps() }>
+                <RichText
+                    tagName="h2"
+                    value={ heading }
+                    onChange={ ( value ) => setAttributes( { heading: value } ) }
+                    placeholder={ __( 'Heading...', 'my-custom-block' ) }
+                />
+                <RichText
+                    tagName="p"
+                    value={ content }
+                    onChange={ ( value ) => setAttributes( { content: value } ) }
+                    placeholder={ __( 'Content...', 'my-custom-block' ) }
+                />
+            </div>
+        </>
+    );
+								   }
